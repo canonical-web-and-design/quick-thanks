81c53a0ea709dab1808d8a9ccfec734f4a19ad9f
minkyngkm <min.kim@canonical.com>
2022-03-02 11:20:30 +0100

    Handle error in post function

--- components/SendButton.tsx
+++ components/SendButton.tsx
@@ -3,25 +3,37 @@ import { Modal } from "@canonical/react-components"
 import { RecognitionProps } from "./Recognition";
 
 const SendButton = ({ users }) => {
-  const [newData, setNewData] = useState<Partial<RecognitionProps>>({
+  const initialValue = {
     authorId: -1,
     recipientId: -1,
     content:"",
     published : true
-  });
+  }
+  const [newData, setNewData] = useState<Partial<RecognitionProps>>(initialValue);
   const [modalOpen, setModalOpen] = useState(false);
 
   const closeHandler = () => setModalOpen(false);
 
   const handleSendButton =  async () => {
-        const res = await fetch('api/recognitions', {
-          method: 'POST',
-          body: JSON.stringify(newData),
-        });
-        const data = await res.json();
-        console.log(data)
-      };
-
+    try {
+      if( newData.authorId === -1 || newData.recipientId === -1 || newData.content.length < 1 ){
+        alert("Check all the inputs")
+        return;
+      } 
+      const res = await fetch('api/recognitions', {
+        method: 'POST',
+        body: JSON.stringify(newData),
+      });
+      const status = await res.status
+      if(status === 200) {
+        alert("Submitted")
+        setNewData(initialValue)
+        setModalOpen(false);
+      }
+    } catch(err){
+      console.error(err);
+    }
+  }
   const handleChange = (e: any) => {
     setNewData({...newData, [e.target.name] : e.target.value })
   }
@@ -53,6 +65,7 @@ const SendButton = ({ users }) => {
               <label>
                 From
                 <select  onChange={handleAuthorChange} name="authorId">
+                  <option value="">choose name</option>
                   {users.map((user) => (
                     <option key={user.id} value={user.id}>{user.fullName}</option>
                   ))}
