a729d215ac1f12b6ec9591b229d2a0092974e85d
Peter Makowski <peter@makowski.uk>
2022-03-03 11:15:06 +0100

    update icon styles

--- pages/user/components/UserSummary.tsx
+++ pages/user/components/UserSummary.tsx
@@ -1,8 +1,6 @@
 import { Col, Row } from "@canonical/react-components";
 import React from "react";
-
-const ExplorerIcon = () => <span>(icon)</span>;
-const VillagerIcon = () => <span>(icon)</span>;
+import { ExplorerIcon, VillagerIcon } from "../../../components/Icons";
 
 const UserSummary = (props) => {
   return (
@@ -10,15 +8,19 @@ const UserSummary = (props) => {
       <Row>
         <Col size={3}>
           <p>
-            <ExplorerIcon /> Explorer points earned
+            <ExplorerIcon color="white" /> Explorer points earned
           </p>
-          <span className="p-heading--2">{props.user?.explorerPoints || 0}</span>
+          <span className="p-heading--2">
+            {props.user?.explorerPoints || 0}
+          </span>
         </Col>
         <Col size={3}>
           <p>
-            <VillagerIcon /> Villager points earned
+            <VillagerIcon color="white" /> Villager points earned
           </p>
-          <span className="p-heading--2">{props.user?.villagerPoints || 0}</span>
+          <span className="p-heading--2">
+            {props.user?.villagerPoints || 0}
+          </span>
         </Col>
       </Row>
     </div>
